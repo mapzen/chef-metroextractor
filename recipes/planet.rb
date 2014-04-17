471c07f130f4cc3da65b7c8ad063ac5ec2ad51e7
Grant Heffernan <heffergm@gmail.com>
2014-04-17 13:22:56 -0400

    fix env var

--- recipes/planet.rb
+++ recipes/planet.rb
@@ -6,7 +6,7 @@
 # override tempfile location so the planet download
 #   temp file goes somewhere with enough space
 #
-# ENV['TMP'] = node[:metroextractor][:basedir]
+ENV['TMP'] = node[:metroextractor][:basedir]
 
 remote_file "#{node[:metroextractor][:basedir]}/#{node[:metroextractor][:planetfile]}" do
   source node[:metroextractor][:planeturl]
