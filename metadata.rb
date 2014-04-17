a2f84321767177773708819050ba9bde7eae5266
Grant Heffernan <heffergm@gmail.com>
2014-04-17 09:42:37 -0400

    version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'GPL v3'
 description      'Installs/Configures extractor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.28'
+version          '0.0.1'
 
 recipe 'extractor', 'Builds and publishes metro extracts'
 
