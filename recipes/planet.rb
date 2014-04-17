991fb90394bfb9abb143cb6a637f34c434cb66aa
Grant Heffernan <heffergm@gmail.com>
2014-04-17 10:24:20 -0400

    specs, fixes

--- recipes/planet.rb
+++ recipes/planet.rb
@@ -6,11 +6,9 @@
 # override tempfile location so the planet download
 #   temp file goes somewhere with enough space
 #
-ENV['TMP'] = node[:metroextractor][:basedir]
+# ENV['TMP'] = node[:metroextractor][:basedir]
 
 remote_file "#{node[:metroextractor][:basedir]}/#{node[:metroextractor][:planetfile]}" do
-  source              node[:metroextractor][:planeturl]
-  use_conditional_get true
-  use_etag            true
-  mode                0644
+  source node[:metroextractor][:planeturl]
+  mode   0644
 end
--- spec/planet_spec.rb
+++ spec/planet_spec.rb
@@ -13,8 +13,9 @@ describe 'metroextractor::planet' do
   end
 
   it 'should download the planet' do
-    chef_run.should run_bash('download planet').with(
-      cwd: '/mnt'
+    chef_run.should create_remote_file('/mnt/planet-latest.osm.pbf').with(
+      source: 'http://ftp.heanet.ie/mirrors/openstreetmap.org/pbf/planet-latest.osm.pbf',
+      mode:   0644
     )
   end
 
