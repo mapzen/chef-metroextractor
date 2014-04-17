aa46fec57692b6532713040795f01d27a09d25bb
Grant Heffernan <heffergm@gmail.com>
2014-04-17 10:13:58 -0400

    use remote_file instead of wget

--- recipes/planet.rb
+++ recipes/planet.rb
@@ -3,11 +3,12 @@
 # Recipe:: planet
 #
 
-bash 'download planet' do
-  cwd  node[:metroextractor][:basedir]
-  code <<-EOH
-    wget --quiet #{node[:metroextractor][:planeturl]} \
-    && chmod +r #{node[:metroextractor][:planetfile]}
-  EOH
-  not_if  { ::File.exist?("#{node[:metroextractor][:basedir]}/#{node[:metroextractor][:planetfile]}") }
+# override tempfile location so the planet download
+#   temp file goes somewhere with enough space
+#
+ENV['TMP'] = node[:metroextractor][:basedir]
+
+remote_file "#{node[:metroextractor][:basedir]}/#{node[:metroextractor][:planetfile]}" do
+  source node[:metroextractor][:planeturl]
+  mode   0644
 end
