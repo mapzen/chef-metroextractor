2e125242a2e5b01199fc0c8c10c07a4f1ae5f3c4
Grant Heffernan <heffergm@gmail.com>
2014-07-29 22:28:04 -0400

    Revert "changelog"
    This reverts commit c04db80ae34d5379b8ca35a2cde0c72e34ff6fec.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'GPL v3'
 description      'Installs/Configures extractor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.13.1'
+version          '0.13.0'
 
 recipe 'metroextractor', 'Builds metro extracts'
 
