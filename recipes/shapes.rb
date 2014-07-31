6acc8fb971a5767ce077b2459251455fb43aeeb3
Grant Heffernan <heffergm@gmail.com>
2014-07-31 07:05:34 -0400

    parallel fixes

--- recipes/shapes.rb
+++ recipes/shapes.rb
@@ -7,12 +7,10 @@ file "#{node[:metroextractor][:setup][:basedir]}/.osm2pgsql.lock" do
   action :nothing
 end
 
-bash 'osm2pgsql' do
+execute 'osm2pgsql' do
   user node[:metroextractor][:user][:id]
   cwd  node[:metroextractor][:setup][:basedir]
-  code <<-EOH
-    parallel --jobs #{node[:metroextractor][:shapes][:osm2pgsql_jobs]} -a #{node[:metroextractor][:setup][:scriptsdir]}/osm2pgsql.sh -d ';' --joblog #{node[:metroextractor][:setup][:basedir]}/logs/paralle_osm2pgsql.log
-  EOH
+  command "#{node[:metroextractor][:setup][:scriptsdir]}/osm2pgsql.sh"
   timeout   node[:metroextractor][:shapes][:osm2pgsql_timeout]
   notifies  :create, "file[#{node[:metroextractor][:setup][:basedir]}/.osm2pgsql.lock]", :immediately
   not_if    { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/.osm2pgsql.lock") }
