0e44442697b5025cd7b9a332d0e1789fa080f8c9
Grant Heffernan <heffergm@gmail.com>
2015-01-05 09:39:37 -0500

    Revert "specs"
    This reverts commit d044c453b12bfb4100b14ea115a7ffa4794e53a5.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'metroextractor::user' do
   before do
     stub_command('pgrep postgres').and_return(true)
-    stub_command('test -f /var/lib/postgresql/9.4/main/PG_VERSION').and_return(true)
+    stub_command('test -f /var/lib/postgresql/9.3/main/PG_VERSION').and_return(true)
     stub_command("psql -c \"SELECT rolname FROM pg_roles WHERE rolname='osmuser'\" | grep osmuser").and_return(true)
     stub_command("psql -c \"SELECT datname from pg_database WHERE datname='osm'\" | grep osm").and_return(true)
     stub_command("psql -c 'SELECT lanname FROM pg_catalog.pg_language' osm | grep '^ plpgsql$'").and_return(true)
