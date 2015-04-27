e5c5d3820eb380577f5916f204282ba74be6a2bf
Grant Heffernan <heffergm@gmail.com>
2015-04-27 13:48:14 -0400

    specs

--- spec/extracts_spec.rb
+++ spec/extracts_spec.rb
@@ -26,7 +26,7 @@ describe 'metroextractor::extracts' do
       user:         'metro',
       cwd:          '/mnt/metro',
       environment:  { 'JAVACMD_OPTIONS' => '-server -XX:SurvivorRatio=8 -Xms15G -Xmx15G' },
-      code:         "    parallel -a /opt/metroextractor-scripts/osmosis.sh -d ';' --joblog /mnt/metro/logs/parallel_osmosis.log\n",
+      code:         "    parallel -j 6 -a /opt/metroextractor-scripts/osmosis.sh -d ';' --joblog /mnt/metro/logs/parallel_osmosis.log\n",
       timeout:      172_800
     )
   end
