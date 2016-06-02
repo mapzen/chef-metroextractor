a0aa1ea3f6bec266acb8c8cf94f3e05392803d7b
Grant Heffernan <heffergm@gmail.com>
2016-06-02 14:52:04 -0400

    pg cookbook upgrade

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -24,7 +24,7 @@ directory node[:postgresql][:data_directory] do
   owner   'postgres'
 end
 
-pg_user node[:metroextractor][:postgres][:user] do
+postgresql_user node[:metroextractor][:postgres][:user] do
   privileges         superuser: true, createdb: true, login: true
   encrypted_password node[:metroextractor][:postgres][:password]
 end
@@ -35,13 +35,13 @@ pg_database node[:metroextractor][:postgres][:db] do
   action :drop
 end
 
-pg_database node[:metroextractor][:postgres][:db] do
+postgresql_database node[:metroextractor][:postgres][:db] do
   owner     node[:metroextractor][:postgres][:user]
   encoding  'utf8'
   template  'template0'
 end
 
-pg_database_extensions node[:metroextractor][:postgres][:db] do
+postgresql_extension node[:metroextractor][:postgres][:db] do
   extensions  ['hstore']
   languages   'plpgsql'
   postgis     true
